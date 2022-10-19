8ed22b2f553d286dd9634625e930fd57b433abd1
menuhwang <hmin0206@naver.com>
2022-10-19 23:03:15 +0900

    메소드 작성 연습

---
+++ minwoo/method/MethodPracticeTest.java
@@ -0,0 +1,11 @@
+package method;
+
+public class MethodPracticeTest {
+    public static void main(String[] args) {
+        MethodPractice methodPractice = new MethodPractice();
+
+        String greet = methodPractice.greet("짱구", 5);
+
+        System.out.println(greet);
+    }
+}
